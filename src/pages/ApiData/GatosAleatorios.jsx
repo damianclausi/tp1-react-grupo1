f3049326138cd5793b6b28108dff48e56b2ceb3c
Antonio <antonio02c@gmail.com>
2025-05-28 22:06:15 -0300

    Se agregan mas Apis publicas

---
+++ src/pages/ApiData/GatosAleatorios.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+export default function GatosAleatorios() {
+  const [gatos, setGatos] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const obtenerGatos = async () => {
+      try {
+        const res = await fetch(
+          "https://api.thecatapi.com/v1/images/search?limit=3"
+        );
+        if (!res.ok) throw new Error("Error en la respuesta de la API");
+        const data = await res.json();
+        setGatos(data.slice(0, 2));
+      } catch (err) {
+        console.error("Error al obtener gatos:", err);
+        setError("Error al obtener imágenes de gatos");
+      }
+    };
+
+    obtenerGatos();
+  }, []);
+
+  if (error) return <p>{error}</p>;
+  if (gatos.length === 0) return <p>Cargando gatos...</p>;
+
+  return (
+    <div>
+      <h2>🐱 Gatito Aleatorio</h2>
+      <ul
+        style={{ listStyle: "none", padding: 0, display: "flex", gap: "10px" }}
+      >
+        {gatos.map((gato, index) => (
+          <li key={index}>
+            <img
+              src={gato.url}
+              alt="Gato aleatorio"
+              style={{ width: "250px", borderRadius: "10px" }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
